ea641c2f23bb384c90c1f70b0ed7804a18077908
DhruvShah <dhruvshah78223@gmail.com>
2024-07-29 13:16:11 +0530

    dockerize the microservice's using docker compose files.

--- jobms/jobms/src/main/java/com/example/jobms/job/clients/ReviewClient.java
+++ jobms/jobms/src/main/java/com/example/jobms/job/clients/ReviewClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "REVIEWMS")
+@FeignClient(name = "REVIEWMS",url="${review-service.url}")
 public interface ReviewClient {
     @GetMapping("/reviews")
     List<Review> getReviews(@RequestParam("companyId") Long companyId);
